9c21462f23c0086f0076e0f0b0a5e37ddd9f96cc
Adrià Alba <146344234+adrialba02@users.noreply.github.com>
2025-02-19 20:31:49 +0100

    Conveyor

---
+++ Assets/Projects/2025/DAM_AJEI/G_4/Points.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Points : MonoBehaviour
+{
+    public static Points Instance;
+
+
+    public int currentPoints;
+    public int hp;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        hp = 3;
+    }
+
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            Application.Quit();
+        }
+
+    }
+}
